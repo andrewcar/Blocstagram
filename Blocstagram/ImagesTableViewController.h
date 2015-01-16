f2b635eec3c9aa6148a2541c26067ffe5a4cde50
Andrew Carvajal <andrewcarvajal1@gmail.com>
2015-01-15 19:07:13 -0500

    Table View controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Andrew Carvajal on 1/14/15.
+//  Copyright (c) 2015 graffme, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
